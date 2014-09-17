8d14b7a3e7b6077821b7ca8f2cb9688108b27907
Artur Huletski <hatless.fox@gmail.com>
2014-09-17 13:30:23 +0400

    Week2. Tuples examples

---
+++ python_2/functions.py
@@ -0,0 +1,41 @@
+def fun_with_tuples():
+    print("Fun with tuples");
+    # Basics
+    t1 = 5, 2; # create via packing
+    print("\tPacking: ", t1);
+    a, b = t1; # unpacking
+    print("\tUnpacking: ", a, b);
+
+    a, b = b, a # swap w/o temp. packing + unpacking
+    print("\tUnpacking:", a, b);
+
+    a, b = 1, 2;
+    a, b = b, a = a, b;
+    print("\tDouble swap: ", a, b); # a = 2, b = 1. Ask me if surprised
+
+    t = 3,
+    print("\tSinge element tuple: ", t);
+    print("\tTuple size: ", len( (a, b, "ping", (3, 7)) ));
+
+    t = 1, "two", (1, 1)
+    print("\tElem access: ", t[0], t[-1], t[2][0]);
+    for i, el in enumerate(t):
+        print("\t {} -> {}".format(i, el))
+    # t[3+] -- Index Error
+    # t[0] = 3 -- Tuples are immutable, can't change element
+    t = "", []
+    t[1].append(42)
+    print("\tTuples are immutable, but elements are mutable: ", t);
+    # t[0] += "a" -- Error: You tell we why
+    # Hint: t[1] += [42] -- Error
+
+def multi_value_return(a, b):
+    if b != 0:
+        return a / b, b % a; # tuple packing
+    else:
+        return "Divisor is 0", "You is stupid" # tuple packing
+
+if __name__ == "__main__":
+    fun_with_tuples();
+#    print(multi_value_return(5, 2));
+#    print(multi_value_return(5, 0));
