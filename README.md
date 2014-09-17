4a626937cab00231245b8a92eef2a89a68a7f23f
Artur Huletski <hatless.fox@gmail.com>
2014-09-17 18:09:46 +0400

    Add more tuple examples; Alter readme

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ Programming-Paradigms
 =====================
 
 Examples.
+
+## What if I spot an error?
+
+1. Be proud, you're careful!
+
+2. Create an issue and describe spotted error.
+
+## What if I know/want extra example(s)?
+
+1. Be proud, you're curious!
+
+2. Create an issue and describe what you want to be added.
--- python_2/functions.py
+++ python_2/functions.py
@@ -1,3 +1,4 @@
+
 def fun_with_tuples():
     print("Fun with tuples");
     # Basics
@@ -7,27 +8,38 @@ def fun_with_tuples():
     print("\tUnpacking: ", a, b);
 
     a, b = b, a # swap w/o temp. packing + unpacking
-    print("\tUnpacking:", a, b);
+    print("\tSwap:", a, b);
 
     a, b = 1, 2;
     a, b = b, a = a, b;
-    print("\tDouble swap: ", a, b); # a = 2, b = 1. Ask me if surprised
+    print("\tDouble swap: ", a, b); # a = 2, b = 1. Look for explanation in repo
 
-    t = 3,
+    t = 3,;
     print("\tSinge element tuple: ", t);
     print("\tTuple size: ", len( (a, b, "ping", (3, 7)) ));
 
-    t = 1, "two", (1, 1)
+    t = 1, "two", (1, 1);
     print("\tElem access: ", t[0], t[-1], t[2][0]);
     for i, el in enumerate(t):
-        print("\t {} -> {}".format(i, el))
+        print("\t {} -> {}".format(i, el));
     # t[3+] -- Index Error
     # t[0] = 3 -- Tuples are immutable, can't change element
-    t = "", []
-    t[1].append(42)
+    t = "", [];
+    t[1].append(42);
     print("\tTuples are immutable, but elements are mutable: ", t);
     # t[0] += "a" -- Error: You tell we why
     # Hint: t[1] += [42] -- Error
+
+    a, b, *c = 1, 2, 3, 4, 5;
+    print("\t\"Unpack\" (non-empty tail): ", a, b, c);
+    a, b, *c = 1, 2;
+    print("\t\"Unpack\" (empty tail case): ", a, b, c);
+    # a, b, *c, *d = 1, 2, 3 -- Error
+
+    a, b, c = [1, 2, 3];
+    print("\tList \"unpacking\": ", a, b, c);
+    # a, b = [1,2,3] -- Error
+
 
 def multi_value_return(a, b):
     if b != 0:
@@ -35,7 +47,10 @@ def multi_value_return(a, b):
     else:
         return "Divisor is 0", "You is stupid" # tuple packing
 
+# TODO: functions
+# TODO: modules
+
 if __name__ == "__main__":
     fun_with_tuples();
-#    print(multi_value_return(5, 2));
+    a, b = multi_value_return(5, 2);
 #    print(multi_value_return(5, 0));
