112af6a6e9dfee3f7f65cf7d5fb5d941694c7cde
Andronik Ordian <andronik.ordian@gmail.com>
2015-09-17 20:14:23 +0300

    help students to solve hw 2

--- python_1/modules/hw_intro.py
+++ python_1/modules/hw_intro.py
@@ -1,17 +1,27 @@
 # dir(<module_name>) -- list content "names" defined by module
 
-import math; # import entire modure
-from os import walk; # import singe "name" (function/submodule)
-from os import path;
-from hashlib import sha1 as cryptohasher; # import name with synonym
+import math  # import entire modure
+from os import walk  # import singe "name" (function/submodule)
+from os import path
+from hashlib import sha1 as cryptohasher  # import name with synonym
 
-print("Sqrt: ", math.sqrt(9));
-print("Traversal:");
+print("Sqrt: ", math.sqrt(9))
+print("Traversal:")
 for _, _, files in walk("../"):
-    print("\t", files);
+    print("\t", files)
 
-hasher = cryptohasher();
-hasher.update(b"chunk1");
-hasher.update(b"chunk2");
-hasher.update("chunk3".encode('utf-8'));
-print(hasher.hexdigest());
+hasher = cryptohasher()
+hasher.update(b"chunk1")
+hasher.update(b"chunk2")
+hasher.update("chunk3".encode('utf-8'))
+print(hasher.hexdigest())
+
+# to get encoding of file, use encoding method
+# with open(filename) as f:
+#     print(f.encoding)
+# to open file with binary mode, use mode = 'rb'
+# with open(filename, mode='rb') as f:
+#     hasher = cryptohasher()
+#     hasher.update(f.read(1024))
+# return hasher.digest() # digest of first min(1024, filesize) bytes of
+# file
