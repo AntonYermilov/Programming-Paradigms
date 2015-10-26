e8aa9a861062edd40111331409f5459024f93e2c
Andronik Ordian <andronik.ordian@gmail.com>
2015-10-27 01:36:41 +0300

    pure evil

--- haskell_1/impure.c
+++ haskell_1/impure.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
-bool flag;
+int flag;
 
 int f(int n) {
     int retVal;
-    if flag then retVal = 2 * n;
-            else retVal = n;
+    if (flag) retVal = 2 * n;
+    else      retVal = n;
     flag = !flag;
     return retVal;
 }
 
-void test(){
-    flag = true;
-    
+int test() {
+    flag = 1;
+
     printf("f(1) + f(2) = %d\n", f(1) + f(2));
     printf("f(2) + f(1) = %d\n", f(2) + f(1));
 
     return 0;
 }
 
-void main(void) {
+int main(void) {
     return test();
 }
 
