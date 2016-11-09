67c8e4b80595eb18e0efef3843d486113bf21137
Andronik Ordian <write@reusable.software>
2016-11-09 12:36:07 +0300

    poker example

---
+++ haskell_2/Bookstore.hs
@@ -0,0 +1,76 @@
+module Books where
+
+data BookInfo = Book Int String [String] deriving (Show)
+data MagazineInfo = Magazine Int String [String] deriving (Show)
+
+type CustomerID = Int
+type ReviewBody = String
+data BookReview = BookReview BookInfo CustomerID ReviewBody
+
+type BookRecord = (BookInfo, BookReview)
+
+type CardHolder = String
+type CardNumber = String
+type Address = [String]
+
+
+data BillingInfo = CreditCard CardNumber CardHolder Address
+                 | CashOnDelivery
+                 | Invoice CustomerID
+                   deriving (Show)
+
+bookID      (Book id _     _       ) = id
+bookTitle   (Book _  title _       ) = title
+bookAuthors (Book _  _      authors) = authors
+
+data Customer = Customer {
+    customerID      :: CustomerID
+  , customerName    :: String
+  , customerAddress :: Address
+  } deriving (Show, Read)
+
+
+customer1 :: Customer
+customer1 = Customer 271828 "J.R. Hacker"
+            ["255 Syntax Ct",
+             "Milpitas, CA 95134",
+             "USA"]
+customer2 :: Customer
+customer2 = Customer {
+              customerID = 271829
+            , customerAddress = ["1048576 Disk Drive",
+                              "Milpitas, CA 95134",
+                              "USA"]
+            , customerName = "Jane Q. Citizen"
+            }
+
+onlyOne :: Customer -> Customer
+onlyOne x@Customer { customerID = 271828 } = x
+-- onlyOne (Human id name address) = show id
+
+-- for fun
+type Question   = String
+type Answer     = String
+type Task       = String
+type TaskAnswer = String
+type TaskScore  = Float
+
+class Student a where
+  question :: a -> Question
+  doHomework :: a -> Maybe TaskAnswer
+
+class Teacher a where
+  answer :: a -> Question -> Maybe Answer
+  checkHomework :: a -> Maybe TaskAnswer -> TaskScore
+
+data Freshman = Freshman { myName :: String, myQuestion :: Question }
+data PhdStudent = PhdStudent { phdName :: String }
+
+instance Student Freshman where
+  question = myQuestion
+  doHomework _ = Nothing
+
+instance Teacher PhdStudent where
+  answer _ _ = Nothing
+  checkHomework _ Nothing = 0
+  checkHomework _ (Just ans) = 0.42 * (fromIntegral . length . words $ ans)
---
+++ haskell_2/poker/Hands.hs
@@ -0,0 +1,49 @@
+module Hands where
+
+import           Data.Function
+import           Data.List
+import           Data.Ord
+
+import           Types
+
+value :: [Card] -> (HandRank, [Card])
+value h = maybe ifNotFlush ifFlush (maybeFlush h)
+  where ifFlush cs = maybe (Flush, take 5 cs) (StraightFlush,) (maybeStraight cs)
+        ifNotFlush = maybe (checkGroups h) (Straight,) (maybeStraight h)
+
+maybeFlush :: [Card] -> Maybe [Card]
+maybeFlush cs = undefined
+
+maybeStraight :: [Card] -> Maybe [Card]
+maybeStraight cs = if length cs'' >= 5 then Just (lastN 5 cs'') else maybeWheel cs'
+  where cs' = nubBy ((==) `on` rank) cs
+        cs'' = head $ sortByLength $ groupBySucc $ sort cs'
+
+maybeWheel :: [Card] -> Maybe [Card]
+maybeWheel cs = if length cs' == 5 then Just cs' else Nothing
+  where cs' = filter (flip elem [Ace, Two, Three, Four, Five] . rank) cs
+
+checkGroups :: [Card] -> (HandRank, [Card])
+checkGroups h = (hr, cs)
+  where gs = sortByLength $ groupBy ((==) `on` rank) $ sort h
+        cs = take 5 $ concat gs
+        hr = case map length gs of
+                  (4:_)    -> Quads
+                  (3:2:_)  -> FullHouse
+                  (3:_)    -> Trips
+                  (2:2:_)  -> TwoPair
+                  (2:_)    -> Pair
+                  _        -> HighCard
+
+groupBySucc :: [Card] -> [[Card]]
+groupBySucc = foldr f []
+  where f a [] = [[a]]
+        f a xs@(x:xs') = if succ (rank a) == rank (head x)
+                         then (a:x):xs'
+                         else [a]:xs
+
+sortByLength :: Ord a => [[a]] -> [[a]]
+sortByLength = reverse . sortBy (comparing length)
+
+lastN :: Int -> [a] -> [a]
+lastN n xs = drop (length xs - n) xs
